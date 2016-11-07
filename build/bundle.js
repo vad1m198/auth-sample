857d8ece0f8f44c986852fe708895ded87a7fa7e
Vadim Merkotan <vmerkotan@dynamo-kiev.local>
2016-11-07 14:43:11 +0200

    update bundle js

--- build/bundle.js
+++ build/bundle.js
@@ -55,17 +55,22 @@
 	
 	var ClientOAuth2 = __webpack_require__(2);
 	
-	console.log("app js configured.......");
-	
-	var githubAuth = new ClientOAuth2({
-	  clientId: 'abc',
-	  clientSecret: '123',
-	  accessTokenUri: 'https://github.com/login/oauth/access_token',
-	  authorizationUri: 'https://github.com/login/oauth/authorize',
-	  authorizationGrants: ['credentials'],
+	var bitbucketAuth = new ClientOAuth2({
+	  clientId: 'VReDDtftrDyQyAdyJL',
+	  clientSecret: 'bbTdBTne7LTLtUev2ANSzuQVesJAg5RR',
+	  accessTokenUri: 'https://bitbucket.org/site/oauth2/access_token',
+	  authorizationUri: 'https://bitbucket.org/site/oauth2/authorize',
+	  //authorizationGrants: ['code'],
 	  redirectUri: 'http://example.com/auth/github/callback',
-	  scopes: ['notifications', 'gist']
+	  scopes: ['account', 'repository']
 	});
+	
+	function CreateToken() {
+	  console.log("createToken");
+	  // Can also just pass the raw `data` object in place of an argument.
+	  var token = bitbucketAuth.createToken('access token', 'optional refresh token', 'optional token type', { data: 'raw user data' });
+	  console.log(token);
+	}
 
 /***/ },
 /* 2 */
