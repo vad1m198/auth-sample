ab1f6a069ed7259fa1a85c8c43d69a14a78f5a3d
Vadim Merkotan <vmerkotan@dynamo-kiev.local>
2016-11-15 14:35:26 +0200

    getTeamRepositories buttons

--- build/bundle.js
+++ build/bundle.js
@@ -36791,7 +36791,7 @@
 /* 13 */
 /***/ function(module, exports) {
 
-	module.exports = "<h3>This is Home Component</h3>\r\n<h4>Hello: {{$ctrl.user.display_name}}</h4>\r\n<button ng-click=\"$ctrl.getUserTeams()\">Get User Teams</button>\r\n<ul>\r\n    <li ng-repeat=\"(uuid, team) in $ctrl.userTeams\">{{team.display_name}}</li>\r\n</ul>\r\n"
+	module.exports = "<h3>This is Home Component</h3>\r\n<h4>Hello: {{$ctrl.user.display_name}}</h4>\r\n<button ng-click=\"$ctrl.getUserTeams()\">Get User Teams</button>\r\n<ul>\r\n    <li ng-repeat=\"(uuid, team) in $ctrl.userTeams\">{{team.display_name}}</li>\r\n</ul>\r\n\r\n<button ng-click=\"$ctrl.getTeamRepositories()\">Get Team Repositories</button>\r\n\r\n"
 
 /***/ },
 /* 14 */
