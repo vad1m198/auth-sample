777d70fc62a28cc1edffb455c6d8860f4af3426b
vad1m198 <persidskij_on_ferrari@ukr.net>
2016-11-19 22:10:05 +0200

    add simple chart example

--- src/components/Home/Team/Project/Repo/repo.controller.js
+++ src/components/Home/Team/Project/Repo/repo.controller.js
@@ -2,13 +2,44 @@ class RepoController {
     constructor(HomeSvc) {
         this.HomeSvc = HomeSvc;
         this.repo = this.HomeSvc.getRepos()
-                              .find(p => p.name = this.slRepoName);     
+                      .find(p => p.name = this.slRepoName);     
         if(this.repo) {              
             this.HomeSvc.getDataByLink(this.repo.links.commits.href.replace(/'/g, "")).then( response => {
                 console.log("getRepoCommits", response)
                 this.HomeSvc.setCommits(response.values);  
             });
         }
+
+        
+  this.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  this.series = ['Series A', 'Series B'];
+  this.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  this.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+ this.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  this.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
+
     }
 
     getCommits() {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import angularChart from 'angular-chart.js';
 import AuthModule from './components/Auth/auth.module.js'
 import HomeModule from './components/Home/home.module.js'
 import 'normalize.css';
 
-angular.module('auth-sample', [uiRouter, AuthModule, HomeModule]);
+angular.module('auth-sample', [uiRouter, AuthModule, HomeModule, angularChart])
+    .config(['ChartJsProvider', function (ChartJsProvider) {
+    // Configure all charts
+    ChartJsProvider.setOptions({
+       colors: ['#97BBCD', '#DCDCDC', '#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
+      responsive: false
+    });
+    // Configure all line charts
+    ChartJsProvider.setOptions('line', {
+      showLines: true
+    });
+  }]);
